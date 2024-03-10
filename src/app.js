0d8dd130fd099508cb5a951152c72c8f28ccbd7a
David Aguilar <davidaguilar5034@gmail.com>
2024-03-09 23:34:08 -0600

    iniciando el proyecto con la conexion a BD

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql2')
+const myConnection = require('express-myconnection');
+
+const app = express();
+
+//importando rutas
+const routes = require('./routes/customer');
+
+//configuración 
+app.set('port', process.env.PORT || 3000);
+app.set('view engine','pug');
+app.set('views', path.join(__dirname,'views'))
+
+//middlewares
+app.use(morgan('dev'));
+app.use(myConnection(mysql,{
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: 3306,
+    database: 'crud_nodejs_mysql'
+},'single'));
+
+
+//routes
+app.use('/', routes);
+
+//archivos estaticos 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+    console.log("Servidor en puerto 3000...")
+})
---
+++ src/controllers/customerController.js
@@ -0,0 +1,7 @@
+const controller = {};
+
+controller.listar = (req,res) => {
+    res.send('hola');
+}
+
+module.exports = controller;
